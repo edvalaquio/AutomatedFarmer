ed8241672438b06c2acfdf74b4a764acd3107a29
LasingNaJedi69 <nuj101nuj@gmail.com>
2018-02-19 01:57:36 +0800

    modifying sensor file.

--- sensor.js
+++ sensor.js
@@ -2,32 +2,50 @@ var Gpio = require('onoff').Gpio;
 var sleep = require('sleep');
 
 var trigger = new Gpio(18, 'out');
-var echo = new Gpio(24, 'in')
+var echo = new Gpio(24, 'in');
 
 var distance = function(){
 	trigger.writeSync(1);
-	sleep.msleep(1);
+	sleep.usleep(10);
 
 	trigger.writeSync(0);
-	startTime = new Date();
-	stopTime = new Date();
+	
+	//~ startTime = new Date();
+	//~ stopTime = new Date();
+	var startTime;
+	var stopTime;
 
-	while echo.readSync() == 0{
-		startTime = new Date();
+	while(echo.readSync() == 0){
+		var temp = new Date();
+		startTime = temp.getTime() / 1000;
 	}
 
-	while echo.readSync() == 1{
-		stopTime = new Date();
+	while(echo.readSync() == 1){
+		var temp = new Date();
+		stopTime = temp.getTime() / 1000;
 	}
 
-	timeElapsed = stopTime - startTime;
-	something = (timeElapsed * 34300) / 2;
+	var timeElapsed = stopTime - startTime;
+	var something = (timeElapsed * 34300) / 2;
 
 	return something;
 }
 
-console.log(distance);
-console.log(distance);
-console.log(distance);
-console.log(distance);
+//~ console.log(distance);
+//~ console.log(distance);
+//~ console.log(distance);
+//~ console.log(distance);
 
+//~ var sample = setInterval
+
+var count = 0;
+while(true){
+	var temp = distance();
+	console.log(temp);
+	
+	sleep.sleep(1)
+	if(count == 10){
+		break;
+	}
+	count++;
+}
