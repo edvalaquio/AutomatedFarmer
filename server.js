45d7bf16ebb49999e4020d17ec11a853dd2344fb
Chloie Marba <hanchloie22@gmail.com>
2018-02-19 08:19:26 +0800

    front-end part

--- server.js
+++ server.js
@@ -11,32 +11,32 @@ server.listen(port, function(){
 app.use(express.static(path.join(__dirname, 'public')));
 
 var numUsers;
-var stepper = require('./stepper');
-var motors = new stepper(2, 3, 4, 17, 27, 22, 10, 9);
-io.on('connection', function(socket){
-	numUsers++;
-	console.log("A user has connected.");
-	socket.on('disconnect', function(){
-		console.log('User disconnected.');
-		numUsers--;
-	});
-	socket.on('move', function(direction){
-		motors.clearPins();
-		if(direction == "up"){
-			motors.forward()
-		} else if(direction == "down"){
-			motors.reverse()
-		} else if(direction == "left"){
-			motors.left()
-		} else if(direction == "right"){
-			motors.right()
-		}
-		console.log("Tractor is moving: " + direction);
-	});
-	socket.on('stop', function(){
-		motors.clearPins();
-	});
-});
+// var stepper = require('./stepper');
+// var motors = new stepper(2, 3, 4, 17, 27, 22, 10, 9);
+// io.on('connection', function(socket){
+// 	numUsers++;
+// 	console.log("A user has connected.");
+// 	socket.on('disconnect', function(){
+// 		console.log('User disconnected.');
+// 		numUsers--;
+// 	});
+// 	socket.on('move', function(direction){
+// 		motors.clearPins();
+// 		if(direction == "up"){
+// 			motors.forward()
+// 		} else if(direction == "down"){
+// 			motors.reverse()
+// 		} else if(direction == "left"){
+// 			motors.left()
+// 		} else if(direction == "right"){
+// 			motors.right()
+// 		}
+// 		console.log("Tractor is moving: " + direction);
+// 	});
+// 	socket.on('stop', function(){
+// 		motors.clearPins();
+// 	});
+// });
 
 // app.use(function(req, res, next) {
 // 	console.log(`${req.method} request for '${req.url}'`);
