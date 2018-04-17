ac55d81f1a0cab718aa1f38026e4fa1b316f7f31
marbasaur <hanchloie22@gmail.com>
2018-04-17 20:15:17 +0800

    path

--- public/js/controllers/automatedCtrl.js
+++ public/js/controllers/automatedCtrl.js
@@ -4,7 +4,7 @@ angular.module("autoFarm.controllers.autoCtrl", [])
 .controller("autoCtrl", ["$rootScope", "$scope", "$window", "$location",
 	function($rootScope, $scope, $window, $location){
 		// var socket;
-		var socket = io('http://192.168.254.39:3000');
+		var socket = io('http://' + $rootScope.hostAddress + ':3000');
 		console.log("Here in autoCtrl");
 		$scope.area = "none";
 		$scope.xAxis = false;
@@ -12,61 +12,42 @@ angular.module("autoFarm.controllers.autoCtrl", [])
 		$scope.axis = [[]];
 
 		$scope.computeDimensions = function(length, width){
-			$scope.area = length*width;
+			// $scope.area = length*width;
 			$scope.xAxis = computeRange(length);
 			$scope.yAxis = computeRange(width);
+			for(var i = 0; i < length; i++){
+				$scope.axis[i] = [];
+				for(var j = 0; j < width; j++){
+					$scope.axis[i][j] = false;
+				}
+			}
 		}
 
 		$scope.printAxis = function(){
 			console.log($scope.axis);
 		}
 
-		$scope.toggleSelectAll = function(){
-
+		$scope.checkPath = function(){
 			for(var i = 0; i < $scope.xAxis.length; i++){
-				$scope.axis[i] = _.values($scope.axis[i]);
+				if(_.includes($scope.axis[i], true)){
+					$scope.hasPath = true;
+					break;
+				}
+				$scope.hasPath = false;
+			}
+		}
+
+		$scope.toggleSelectAll = function(flag){
+			$scope.hasPath = false;
+			if(flag){
+				$scope.hasPath = flag;
+			}
+			for(var i = 0; i < $scope.xAxis.length; i++){
 				for(var j = 0; j < $scope.yAxis.length; j++){
-					$scope.axis[i][j] = !$scope.axis[i][j];
+					$scope.axis[i][j] = flag;
 				}
 			}
 			console.log($scope.axis);
-			// console.log($scope.axis);
-			// for(var i = 0; i < $scope.xAxis; i++){
-			// 	$scope.axis[i] = _.values($scope.axis[x]);
-			// 	// for(var j = 0; j < $scope.yAxis; j++){
-			// 	// 	$scope.axis[i][j] = true;
-			// 	// }
-			// }
-			// console.log($scope.axis);
-			// console.log(_.values($scope.axis));
-			// console.log(_);
-			// $scope.axis.forEach(function(item, index){
-			// 	// _.values(item);
-			// 	item = _.values(item);
-			// 	console.log(item);
-			// 	item.forEach(function(item1){
-			// 		item1 = true;
-			// 	});
-			// 	// item = Array.from(item);
-			// 	// console.log(item);
-			// 	// item.forEach(function(item1, index){
-			// 	// })
-			// });
-			// console.log($scope.axis);
-			// var boxes = Array.from(document.getElementsByClassName('checkbox-big'));
-			// console.log(boxes);
-
-			// boxes.forEach(function(item, index){
-			// 	item.value = true;
-			// });
-		}
-
-		var checkAll = function(x){
-			var checks = [];
-			for(var i = 0; i < x; i++){
-				checks[i] = true;
-			}
-			return checks;
 		}
 
 		var computeRange = function(value){
@@ -76,36 +57,5 @@ angular.module("autoFarm.controllers.autoCtrl", [])
 			}
 			return array;
 		}
-
-		var initializeAxis = function(x, y){
-			var temp = [[]];
-			for(var i = 0; i < x; i++){
-				for(var j = 0; j < y; j++){
-					temp[i][j] = true;
-				}
-			}
-			return temp;
-		}
-
-		// $scope.sample = false;
-		// $scope.move = function(direction){
-		// 	console.log(direction);
-		// 	socket.emit('move', direction);
-		// }
-
-		// var timeout;
-		// $scope.buttonPressed = function(direction){
-		// 	//~ console.log(direction);
-		// 	$scope.pressed = true;
-		// 	timeout = setInterval(function(){
-		// 		socket.emit('move', direction);
-		// 	}, 0.5);
-		// };
-
-		// $scope.buttonReleased = function(){
-		// 	$scope.pressed = false;
-		// 	clearInterval(timeout);
-		// 	socket.emit('stop');
-		// };
 	}	
 ]);
