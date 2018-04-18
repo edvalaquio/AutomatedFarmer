41a123637e702c07c4b88c22214a64cae36dc5d3
marbasaur <hanchloie22@gmail.com>
2018-04-18 22:21:01 +0800

    Pulling hcanges

--- env.js
+++ env.js
@@ -3,11 +3,16 @@ var env = {
 	'port' 	: 3000 
 }
 
+console.log(os.platform())
+console.log(os.networkInterfaces())
+
 var address = os.networkInterfaces();
-if(os.platform() == 'win32'){
+if(address['Wi-Fi']){
 	env.host = address['Wi-Fi'][1].address
-} else {
+} else if(address['wlan0']){
 	env.host = address['wlan0'][0].address
+} else {
+	env.host = '127.0.0.1'
 }
 
 module.exports = env;
