2fcac189117d50aca6c6dc629d838b7c793359ee
LasingNaJedi69 <nuj101nuj@gmail.com>
2018-05-31 11:45:56 +0800

    Pushing parser

--- farmer-modules/gps-socket.io.js
+++ farmer-modules/gps-socket.io.js
@@ -1,28 +1,12 @@
+var SerialPort = require('serialport');
+var Readline = new SerialPort.parsers.Readline;
+var serial = new SerialPort('/dev/ttyAMA0');
+var parser = new ReadLine();
 
-var serial = require('serialport');
-const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('/dev/ttyAMA0')
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
-parser.on('data', console.log)
-// var nmea = require('nmea')
-
+serial.pipe(parser);
+parser.on('data', function(data){
+	console.log(data);
+});
 
 module.exports = function(socket){
-	// var serialport = require("serialport");
-	// var SerialPort = serialport.SerialPort;
-	// var sp = new SerialPort("/dev/ttyAMA0", {
-	// 	baudrate:9600,
-	// 	databits: 8,
-	// 	parity: 'none',
-	// 	stopBits: 1,
-	// 	flowControl: false,
-	// 	parser: serialport.parsers.readline("\n"),
-	// });
-
-	// sp.on('open', function() {
-	// 	console.log('open');
-	// 	sp.on('data', function(data) {
-	// 		console.log('data received: ' + data);
-	// 	});
-	// });
 }
