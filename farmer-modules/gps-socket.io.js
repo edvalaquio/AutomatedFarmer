e9c9094ae26745df312206727c79da92e2a14e9a
LasingNaJedi69 <nuj101nuj@gmail.com>
2018-05-31 11:21:48 +0800

    Fixing serial port issues

--- farmer-modules/gps-socket.io.js
+++ farmer-modules/gps-socket.io.js
@@ -1,25 +1,28 @@
 
 var serial = require('serialport');
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('/dev/ttyAMA0')
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', console.log)
 // var nmea = require('nmea')
 
 
 module.exports = function(socket){
-	var serialport = require("serialport");
-	var SerialPort = serialport.SerialPort;
-	var sp = new SerialPort("/dev/ttyAMA0", {
-		baudrate:9600,
-		databits: 8,
-		parity: 'none',
-		stopBits: 1,
-		flowControl: false,
-		parser: serialport.parsers.readline("\n"),
-	});
+	// var serialport = require("serialport");
+	// var SerialPort = serialport.SerialPort;
+	// var sp = new SerialPort("/dev/ttyAMA0", {
+	// 	baudrate:9600,
+	// 	databits: 8,
+	// 	parity: 'none',
+	// 	stopBits: 1,
+	// 	flowControl: false,
+	// 	parser: serialport.parsers.readline("\n"),
+	// });
 
-	sp.on('open', function() {
-		console.log('open');
-		sp.on('data', function(data) {
-			console.log('data received: ' + data);
-		});
-	});
-
+	// sp.on('open', function() {
+	// 	console.log('open');
+	// 	sp.on('data', function(data) {
+	// 		console.log('data received: ' + data);
+	// 	});
+	// });
 }
